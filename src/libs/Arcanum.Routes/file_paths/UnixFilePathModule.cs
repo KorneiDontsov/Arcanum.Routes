d818506868cfccada0a1730e6165f7b7c67abdf9
Kornei Dontsov <KorneiDontsov@gmail.com>
2019-12-29 00:21:34 +0300

    Implement simple conversions to Unix file paths.

---
+++ src/libs/Arcanum.Routes/file_paths/UnixFilePathModule.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
+
+namespace Arcanum.Routes {
+	using System;
+	using System.Text;
+
+	public static class UnixFilePathModule {
+		public static String ToLocalUnixFilePath (this Route route) {
+			var path = new StringBuilder(128).Append('.');
+			foreach (var node in route.nodes) path.Append('/').Append(node);
+			return path.ToString();
+		}
+
+		public static String ToFullUnixFilePath (this Route route) {
+			var nodeE = route.nodes.GetEnumerator();
+			if (! nodeE.MoveNext()) return "/";
+			else {
+				var path = new StringBuilder(128);
+
+				do path.Append('/').Append(nodeE.Current);
+				while (nodeE.MoveNext());
+
+				return path.ToString();
+			}
+		}
+	}
+}
---
+++ src/tests/Arcanum.Routes.Tests/TestUnixFilePathModule.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Kornei Dontsov. All Rights Reserved. Licensed under the MIT. See LICENSE in the project root for license information.
+
+namespace Arcanum.Routes.Tests {
+	using FluentAssertions;
+	using Xunit;
+
+	public class TestUnixFilePathModule {
+		[Fact]
+		public void RouteConvertedToLocalPath () {
+			Route route = "dir/nested-dir/file.ext";
+			var path = route.ToLocalUnixFilePath();
+			path.Should().Be("./dir/nested-dir/file.ext");
+		}
+
+		[Fact]
+		public void EmptyRouteConvertedToLocalPath () {
+			var route = Route.empty;
+			var path = route.ToLocalUnixFilePath();
+			path.Should().Be(".");
+		}
+
+		[Fact]
+		public void RouteConvertedToFullPath () {
+			Route route = "dir/nested-dir/file.ext";
+			var path = route.ToFullUnixFilePath();
+			path.Should().Be("/dir/nested-dir/file.ext");
+		}
+
+		[Fact]
+		public void EmptyRouteConvertedToFullPath () {
+			var route = Route.empty;
+			var path = route.ToFullUnixFilePath();
+			path.Should().Be("/");
+		}
+	}
+}
